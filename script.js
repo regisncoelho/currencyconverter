f48ed8e2dd4c2aa4aa08cbd8ce5861c9fcf69ad7
Regis Coelho <regis.andre09@gmail.com>
2021-05-20 03:30:30 -0300

    Remodel the design using inputs to receive the values to be converted. Alters index, style and script files. Creates reset.css

--- script.js
+++ script.js
@@ -6,29 +6,34 @@ var CambioPesoReal = 0.060
 
 //Calculadora de conversão
 
-var moeda = prompt("Escolha uma das moedas abaixo:" + "\n" + "• Dolar" + "\n" + "• Euro" + "\n" + "• Peso Argentino")
+document.getElementById('button-converter').onclick = function converter() {
+    var moeda = document.getElementById("input-dolar").value
+    console.log(moeda)
+}
 
-if (moeda == "Dolar" || moeda == "dolar" || moeda == "dólar"){
+
+/*if (moeda == "Dolar" || moeda == "dolar" || moeda == "dólar"){
 var valorDolarInput = parseFloat(prompt("Informe o valor em dólares"))
 var valorEmReal = valorDolarInput * CambioDolarReal
-document.write("<h2>" + "U$" + valorDolarInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
+document.write("<h2 class='result'>" + "U$" + valorDolarInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
 }
 
 else if (moeda == "Euro" || moeda == "euro"){
 var valorEuroInput = parseFloat(prompt("Informe o valor em euros"))
 var valorEmReal = valorEuroInput * CambioEuroReal
-document.write("<h2>" + "₤" + valorEuroInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
+document.write("<h2 class='result'>" + "₤" + valorEuroInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
 }            
 
 else if (moeda == "Peso Argentino" || moeda == "peso argentino" || moeda == "Peso argentino" || moeda == "Peso" || moeda == "peso"){
 var valorPesoInput = parseFloat(prompt("Informe o valor em pesos argentinos"))
 var valorEmReal = valorPesoInput * CambioPesoReal
-document.write("<h2>" + "$" + valorPesoInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
+document.write("<h2 class='result'>" + "$" + valorPesoInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
 }
 
 else {
-  document.write("<h3>" + "Um dos valores foi inserido incorretamente <br> Tente novamente" + "</h3>")
-}
+  document.write("<h3 class='error'>" + "Um dos valores foi inserido incorretamente <br> Tente novamente" + "</h3>")
+}*/
+
 //FIM da calculadora de conversão
 
 //Modelo
