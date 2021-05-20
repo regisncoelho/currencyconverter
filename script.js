fed41cf76e4b69514ed9ed32c6d617e23ff900c5
Regis Coelho <regis.andre09@gmailcom>
2021-05-19 21:42:43 -0300

    First commit containing original project based on the first class of imersao_dev

---
+++ script.js
@@ -0,0 +1,82 @@
+//Base de dados do câmbio
+
+var CambioDolarReal = 5.677
+var CambioEuroReal = 6.534
+var CambioPesoReal = 0.060
+
+//Calculadora de conversão
+
+var moeda = prompt("Escolha uma das moedas abaixo:" + "\n" + "• Dolar" + "\n" + "• Euro" + "\n" + "• Peso Argentino")
+
+if (moeda == "Dolar"){
+var valorDolarInput = parseFloat(prompt("Informe o valor em dólares"))
+var valorEmReal = valorDolarInput * CambioDolarReal
+document.write("<h2>" + "U$" + valorDolarInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
+}    
+
+else if (moeda == "dolar"){
+var valorDolarInput = parseFloat(prompt("Informe o valor em dólares"))
+var valorEmReal = valorDolarInput * CambioDolarReal
+document.write("<h2>" + "U$" + valorDolarInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
+}   
+
+else if (moeda == "dólar"){
+var valorDolarInput = parseFloat(prompt("Informe o valor em dólares"))
+var valorEmReal = valorDolarInput * CambioDolarReal
+document.write("<h2>" + "U$" + valorDolarInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
+} 
+
+else if (moeda == "Euro"){
+var valorEuroInput = parseFloat(prompt("Informe o valor em euros"))
+var valorEmReal = valorEuroInput * CambioEuroReal
+document.write("<h2>" + "₤" + valorEuroInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
+} 
+               
+else if (moeda == "euro"){
+var valorEuroInput = parseFloat(prompt("Informe o valor em euros"))
+var valorEmReal = valorEuroInput * CambioEuroReal
+document.write("<h2>" + "₤" + valorEuroInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
+}               
+
+else if (moeda == "Peso Argentino"){
+var valorPesoInput = parseFloat(prompt("Informe o valor em euros"))
+var valorEmReal = valorPesoInput * CambioPesoReal
+document.write("<h2>" + "$" + valorPesoInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
+}
+
+else if (moeda == "peso argentino"){
+var valorPesoInput = parseFloat(prompt("Informe o valor em euros"))
+var valorEmReal = valorPesoInput * CambioPesoReal
+document.write("<h2>" + "$" + valorPesoInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
+}
+
+else if (moeda == "Peso argentino"){
+var valorPesoInput = parseFloat(prompt("Informe o valor em euros"))
+var valorEmReal = valorPesoInput * CambioPesoReal
+document.write("<h2>" + "$" + valorPesoInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
+}
+
+else if (moeda == "Peso"){
+var valorPesoInput = parseFloat(prompt("Informe o valor em pesos argentinos"))
+var valorEmReal = valorPesoInput * CambioPesoReal
+document.write("<h2>" + "$" + valorPesoInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
+}
+
+else if (moeda == "peso"){
+var valorPesoInput = parseFloat(prompt("Informe o valor em pesos argentinos"))
+var valorEmReal = valorPesoInput * CambioPesoReal
+document.write("<h2>" + "$" + valorPesoInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
+}
+
+else {
+  document.write("<h3>" + "Um dos valores foi inserido incorretamente <br> Tente novamente" + "</h3>")
+}
+//FIM da calculadora de conversão
+
+//Modelo
+/*alert("Resultado: "+ "\n"+"Temperatura digitada (C°): "+ valorCelsius +"°"+ "\n"+ "Celsius para Kelvin: "+ RKelvin + "°" +"\n"+ "Celsius para Fahrenheit: "+ RFah + "°") */
+
+
+// Revisão
+// variáveis var int - float - string (texto entre aspas)
+// funções alert - parseInt - parseFloat - prompt
