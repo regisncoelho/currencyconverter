fe74f02dc1ef4a2a2f98a69f606fa34bd1f58413
Regis Coelho <regis.andre09@gmail.com>
2021-05-21 22:45:18 -0300

    Final version with all the components positioned and new javascript code

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
 //Base de dados do câmbio
 
-var CambioDolarReal = 5.677
-var CambioEuroReal = 6.534
-var CambioPesoReal = 0.060
+var cambioDolarReal = 5.677
+var cambioEuroReal = 6.534
+var cambioPesoReal = 0.060
 
 //Calculadora de conversão
 
-document.getElementById('button-converter').onclick = function converter() {
-    var moeda = document.getElementById("input-dolar").value
-    console.log(moeda)
+document.querySelector('#button-converter').onclick = function converter() {
+  var dolar = document.querySelector("#input-dolar").value
+  var euro = document.querySelector("#input-euro").value
+  var peso = document.querySelector("#input-pesos").value
+
+  if (dolar != 0) {
+    var valorEmReal = dolar*cambioDolarReal
+    const divResultado = document.querySelector(".moeda-output")
+    divResultado.insertAdjacentHTML("beforeend", `<p class="result">${valorEmReal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>`)
+    const inputField = document.querySelector("#input-dolar")
+    inputField.value = ""
+  }
+
+  else if (euro != 0) {
+    var valorEmReal = euro*cambioEuroReal
+    const divResultado = document.querySelector(".moeda-output")
+    divResultado.insertAdjacentHTML("beforeend", `<p class="result">${valorEmReal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>`)
+    const inputField = document.querySelector("#input-euro")
+    inputField.value = ""
+  }
+
+  else if (peso != 0) {
+    var valorEmReal = peso*cambioPesoReal
+    const divResultado = document.querySelector(".moeda-output")
+    divResultado.insertAdjacentHTML("beforeend", `<p class="result">${valorEmReal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>`)
+    const inputField = document.querySelector("#input-pesos")
+    inputField.value = ""
+  }
+
+  else {
+    const divResultado = document.querySelector(".moeda-output")
+    divResultado.insertAdjacentHTML("beforeend", `<p class="error">Ocorreu um erro<br>Tente novamente!</p>`)
+  }
 }
 
-
-/*if (moeda == "Dolar" || moeda == "dolar" || moeda == "dólar"){
-var valorDolarInput = parseFloat(prompt("Informe o valor em dólares"))
-var valorEmReal = valorDolarInput * CambioDolarReal
-document.write("<h2 class='result'>" + "U$" + valorDolarInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
+document.querySelector('#button-clear').onclick = function clear() {
+  let previous = document.querySelector(".moeda-output") 
+  while (previous.firstChild){
+    previous.removeChild(previous.firstChild)
+  }
+  
 }
-
-else if (moeda == "Euro" || moeda == "euro"){
-var valorEuroInput = parseFloat(prompt("Informe o valor em euros"))
-var valorEmReal = valorEuroInput * CambioEuroReal
-document.write("<h2 class='result'>" + "₤" + valorEuroInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
-}            
-
-else if (moeda == "Peso Argentino" || moeda == "peso argentino" || moeda == "Peso argentino" || moeda == "Peso" || moeda == "peso"){
-var valorPesoInput = parseFloat(prompt("Informe o valor em pesos argentinos"))
-var valorEmReal = valorPesoInput * CambioPesoReal
-document.write("<h2 class='result'>" + "$" + valorPesoInput.toFixed(2) + " = " + "R$" + valorEmReal.toFixed(2) + "</h2>")
-}
-
-else {
-  document.write("<h3 class='error'>" + "Um dos valores foi inserido incorretamente <br> Tente novamente" + "</h3>")
-}*/
-
-//FIM da calculadora de conversão
-
-//Modelo
-/*alert("Resultado: "+ "\n"+"Temperatura digitada (C°): "+ valorCelsius +"°"+ "\n"+ "Celsius para Kelvin: "+ RKelvin + "°" +"\n"+ "Celsius para Fahrenheit: "+ RFah + "°") */
-
-
-// Revisão
-// variáveis var int - float - string (texto entre aspas)
-// funções alert - parseInt - parseFloat - prompt
